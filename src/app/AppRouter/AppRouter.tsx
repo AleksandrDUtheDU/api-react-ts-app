763b5e565334c5f3c58a61ea99d0dbb26a9cb053
Parshin <dmitry.parshin@semrush.com>
2023-06-01 22:41:09 +0200

    used enum for route config

--- src/app/AppRouter/AppRouter.tsx
+++ src/app/AppRouter/AppRouter.tsx
@@ -1,14 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { Layout } from "../../pages/Layout/Layout";
-import { HOME_URL, LOGIN_URL, REGIST_URL } from "./pathRouter";
+import { ROUTES } from "./pathRouter";
 
 export function Routing() {
   return (
     <Routes>
-      <Route path={HOME_URL} element={<Layout />}>
+      <Route path={ROUTES.HOME_URL} element={<Layout />}>
         <Route index element={<div>Основное MAIN</div>} />
-        <Route path={LOGIN_URL} element={<div>AuthForm LOGIN</div>} />
-        <Route path={REGIST_URL} element={<div>AuthForm REGIST</div>} />
+        <Route path={ROUTES.LOGIN_URL} element={<div>AuthForm LOGIN</div>} />
+        <Route path={ROUTES.REGIST_URL} element={<div>AuthForm REGIST</div>} />
       </Route>
     </Routes>
   );
